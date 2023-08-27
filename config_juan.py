390ba42b75bb099c8a205f4ad58fd09882b011a4
JuanPLorenzo <elem0110.jpl@gmail.com>
2023-08-28 00:30:20 +0200

    guardar27082023

--- config_juan.py
+++ config_juan.py
@@ -1,8 +1,17 @@
-MAYORIA_EDAD = 18
+'''MAYORIA_EDAD = 18
 ALTURA_MINIMA =150
 LETRA_PROHIBIDA = 'a'
 
-print("Hello")
+print("Hello")'''
+from random import random, seed
+
+#seed()
+
+for i in range(5):
+    print(random())
+
+
+
 
 
 
